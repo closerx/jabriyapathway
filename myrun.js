addf5e9130dc2a3f45e6baeaa46d302ebbf81d4c
Hadi Hakami <74460140+closerx@users.noreply.github.com>
2026-05-13 01:11:36 +0300

    Update myrun.js

--- myrun.js
+++ myrun.js
@@ -1,2 +1,7 @@
-import app from './dist/server.js'; 
+// استيراد السيرفر المترجم
+import * as serverModule from './dist/server.js';
+
+// التحقق من وجود التصدير الصحيح
+const app = serverModule.default || serverModule.app || serverModule;
+
 export default app;
